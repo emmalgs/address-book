fe6d0b3ded34f15900496bbb562bc5bce6bb2c7a
Emma Gerigscott <elgerig@gmail.com>
2023-04-03 21:37:04 -0700

    delete entries using event target id

--- js/scripts.js
+++ js/scripts.js
@@ -64,15 +64,18 @@ function listContacts(addressBookToDisplay) {
 
 function displayContactDetails(event) { 
   const contact = addressBook.findContact(event.target.id);
-  document.querySelector("#first-name").innerText = contact.firstName;
-  document.querySelector("#last-name").innerText = contact.lastName;
-  document.querySelector("#phone-number").innerText = contact.phoneNumber;
+  document.querySelector(".first-name").innerText = contact.firstName;
+  document.querySelector(".last-name").innerText = contact.lastName;
+  document.querySelector(".phone-number").innerText = contact.phoneNumber;
   document.querySelector("button.delete").setAttribute("id", contact.id);
   document.querySelector("div#contact-details").removeAttribute("class");
 }
 
 function handleDelete(event) {
-
+  addressBook.deleteContact(event.target.id);
+  document.querySelector("button.delete").removeAttribute("id");
+  document.querySelector("div#contact-details").setAttribute("class", "hidden");
+  listContacts(addressBook);
 }
 
 function handleFormSubmission(event) {
@@ -83,9 +86,13 @@ function handleFormSubmission(event) {
   let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
   addressBook.addContact(newContact);
   listContacts(addressBook);
+  document.querySelector("input#new-first-name").value = null;
+  document.querySelector("input#new-last-name").value = null;
+  document.querySelector("input#new-phone-number").value = null;
 }
 
 window.addEventListener("load", function () {
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
   document.querySelector("div#contacts").addEventListener("click", displayContactDetails);
+  document.querySelector("button.delete").addEventListener("click", handleDelete);
 });
